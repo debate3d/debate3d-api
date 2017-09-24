fa7cb8222b2c2ed78063ede13211dd0f8560cbf0
Emanuel G <emanuelgdesouza@gmail.com>
2017-09-23 22:24:02 -0300

    Insert dataloader on topic resolver

--- src/graphql-entities/resolvers/topic/relations/position.js
+++ src/graphql-entities/resolvers/topic/relations/position.js
@@ -1,6 +1,3 @@
-module.exports = (root, args, context) => {
-  return context
-    .db('position')
-    .where('id', root.id_position)
-    .first()
+module.exports = ({ id_position }, args, { dataLoader }) => {
+  return dataLoader.position.load(id_position)
 }
--- src/graphql-entities/resolvers/topic/relations/tags.js
+++ src/graphql-entities/resolvers/topic/relations/tags.js
@@ -1,10 +1,8 @@
-module.exports = (topic, args, { db }) => {
+module.exports = (topic, args, { db, dataLoader }) => {
   return db('tags_to_topic')
     .where('uid_topic', topic.uid)
     .then(rows => {
-      const tags = rows.map(row => {
-        return db('tags').where({ uid: row.uid_tag }).first()
-      })
+      const tags = rows.map(row => dataLoader.tag.load(row.uid_tag))
       return Promise.all(tags)
     })
 }
