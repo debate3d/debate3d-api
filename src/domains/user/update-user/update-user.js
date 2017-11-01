12f654b8923c0f87cc2f92b9afaadce7bb0c067c
Emanuel G <emanuelgdesouza@gmail.com>
2017-10-31 23:50:24 -0200

    insert flag is_verified on update user method

--- src/domains/user/update-user/update-user.js
+++ src/domains/user/update-user/update-user.js
@@ -1,11 +1,12 @@
-const { head } = require('lodash')
+const { head, merge } = require('lodash')
 
 const { update } = require('../../../helpers/database')
 const { returnFields } = require('../../../helpers/common')
 
 const updateUser = (data, db, uidUser) => {
   const fields = returnFields(['uid', 'name', 'cpf', 'cep', 'facebook', 'twitter', 'instagram', 'site', 'youtube'], data)
-  return update(db('users'), data, { uid: uidUser }, fields).then(head)
+  const merged = merge(data, { is_verified: true })
+  return update(db('users'), merged, { uid: uidUser }, fields).then(head)
 }
 
 module.exports = updateUser
