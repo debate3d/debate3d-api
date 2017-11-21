d1377b25d45713f4633576515b6ff44413f17c39
EmanuelGSouza <emanuelgdesouza@gmail.com>
2017-11-20 22:56:21 -0200

    - added logic to get user and topic by nickname

--- src/graphql-entities/resolvers/topic/root-query/topic.js
+++ src/graphql-entities/resolvers/topic/root-query/topic.js
@@ -1,3 +1,21 @@
-const topic = (root, { uid }, { dataLoader }) => dataLoader.topics.load(uid)
+const { has, keys, size } = require('lodash')
 
-module.exports = topic
+const user = (root, args, { db, dataLoader }) => {
+  if (size(keys(args)) > 1) {
+    return Promise.reject(new Error('Há mais de um parametro para buscar um tema'))
+  }
+  if (size(keys(args)) > 1) {
+    return Promise.reject(new Error('É esperado um parametro'))
+  }
+  if (has(args, 'nickname')) {
+    return db('topics')
+      .where('nickname', args.nickname)
+      .first()
+  }
+  if (has(args, 'uid')) {
+    return dataLoader.topics.load(args.uid)
+  }
+  return null
+}
+
+module.exports = user
--- src/graphql-entities/resolvers/user/root-query/user.js
+++ src/graphql-entities/resolvers/user/root-query/user.js
@@ -1,3 +1,21 @@
-const user = (root, { uid }, { dataLoader }) => dataLoader.users.load(uid)
+const { has, keys, size } = require('lodash')
+
+const user = (root, args, { db, dataLoader }) => {
+  if (size(keys(args)) > 1) {
+    return Promise.reject(new Error('Há mais de um parametro para buscar um usuário'))
+  }
+  if (size(keys(args)) > 1) {
+    return Promise.reject(new Error('É esperado um parametro'))
+  }
+  if (has(args, 'nickname')) {
+    return db('users')
+      .where('nickname', args.nickname)
+      .first()
+  }
+  if (has(args, 'uid')) {
+    return dataLoader.users.load(args.uid)
+  }
+  return null
+}
 
 module.exports = user
