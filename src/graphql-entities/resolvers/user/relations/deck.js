a18aaa0053bee2684d09932b885a0f82f5c429e0
EmanuelGSouza <emanuelgdesouza@gmail.com>
2018-01-06 19:10:55 -0200

    - prevent erros on deck resolver

--- src/graphql-entities/resolvers/user/relations/deck.js
+++ src/graphql-entities/resolvers/user/relations/deck.js
@@ -1,4 +1,8 @@
+const { isEmpty } = require('lodash')
+
 module.exports = (user, args, context) => {
+  if (isEmpty(user)) return []
+
   return context
     .db('decks_store')
     .where('uid_user', user.uid)
