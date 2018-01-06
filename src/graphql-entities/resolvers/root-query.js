ec892812a3ac5ec3373fea4f511c524adece992d
EmanuelGSouza <emanuelgdesouza@gmail.com>
2018-01-06 19:18:41 -0200

    - added version resolver

--- src/graphql-entities/resolvers/root-query.js
+++ src/graphql-entities/resolvers/root-query.js
@@ -1,1 +1,9 @@
-module.exports = { }
+const packageJson = require('../../../package.json')
+
+const version = (root, args, context) => {
+  return packageJson.version
+}
+
+module.exports = {
+  version
+}
