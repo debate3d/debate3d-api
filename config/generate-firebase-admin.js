0cda39e9bd868f75060061b717ad93a688c7321e
EmanuelGSouza <emanuelgdesouza@gmail.com>
2018-02-13 19:54:13 -0200

    fixed bug on firebase envinronment variable

--- config/generate-firebase-admin.js
+++ config/generate-firebase-admin.js
@@ -9,7 +9,7 @@ const generateFirebaseAdminConfig = () => {
     type: 'service_account',
     project_id: process.env.FIREBASE_PROJECT,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: process.env.FIREBASE_AUTH_URI,
