70285d25cec351f83f46a4280fce6daec626e7bd
EmanuelGSouza <emanuelgdesouza@gmail.com>
2018-02-13 18:18:36 -0200

    update topic resolver

--- src/graphql-entities/resolvers/topic/follow-topic/topic.js
+++ src/graphql-entities/resolvers/topic/follow-topic/topic.js
@@ -1,10 +1,18 @@
 const { isEmpty } = require('lodash')
+const FIELDS = [
+  'uid',
+  'ponts',
+  'title',
+  'nickname',
+  'url_image',
+  'id_position'
+]
 
 const topic = ({ uid_topic }, args, { db }) => {
   if (isEmpty(uid_topic)) return null
 
   return db('topics')
-    .select(['ponts', 'title', 'nickname'])
+    .select(FIELDS)
     .where('uid', uid_topic)
     .first()
 }
